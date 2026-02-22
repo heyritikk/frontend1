4e37cb0470c880494909e7e4990aecf45a7919bf
Ritik Kumar <147754937+heyritikk@users.noreply.github.com>
2026-02-22 15:08:25 +0530

    Initial commit

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { VerifyComponent } from './components/verify/verify.component';
+
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'verify', component: VerifyComponent },
+  { path: '**', redirectTo: '' }
+];
